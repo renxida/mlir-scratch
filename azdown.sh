09f3219fd221a8da8d70f950bd35021cf93b23b3
Xida Ren <xida.ren.dev@gmail.com>
2024-04-01 17:11:05 +0000

    add a bunch of stuff & create a semblence of organization

--- azdown.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/bash
-
-# azdown
-filename=`basename $1`
-az storage copy -s https://sharkblobs.blob.core.windows.net/xida/$filename -d $filename
-
--- azup.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-filename=`basename $1`
-az storage copy -s $1 -d https://sharkblobs.blob.core.windows.net/xida/$filename
